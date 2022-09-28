f5cddac890487f85a752ccf12150cf06fbcb26ac
Maastoren <isaacmcconnell@augustana.edu>
2022-09-28 15:14:16 -0500

    Added dancing and saying goodbye

--- TestProject/src/TestMain.java
+++ TestProject/src/TestMain.java
@@ -4,6 +4,6 @@ public class TestMain
 	public static void main(String[] args)
 	{
 		System.out.println("Hello Chicken World!");
-		System.out.println("Goodbye");
+		System.out.println("Goodbye.");
 	}
 }
